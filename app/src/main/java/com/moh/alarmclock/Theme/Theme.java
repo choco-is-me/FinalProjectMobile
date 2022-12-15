274d7d65c9f688a5739ebe9d19661d2898b531be
thebao2307 <thebao1426@gmail.com>
2022-12-16 00:10:13 +0700

    update UI #7, deleted unneeded strings, update Theme.

--- app/src/main/java/com/moh/alarmclock/Theme/Theme.java
+++ app/src/main/java/com/moh/alarmclock/Theme/Theme.java
@@ -11,20 +11,16 @@ public class Theme {
 
     public static final int LIGHT_THEME = 0;
     public static final int DARK_THEME = 1;
-    public static final int FOLLOW_SYSTEM_THEME = -1;
 
     public static void updateTheme(Context context){
         int themeValue = Integer.parseInt(SharedPref.get(context.getString(R.string.theme_version)
-                ,FOLLOW_SYSTEM_THEME+""));
+                ,LIGHT_THEME+""));
         switch (themeValue){
             case LIGHT_THEME:
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 break;
             case DARK_THEME:
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                break;
-            case FOLLOW_SYSTEM_THEME:
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
                 break;
         }
     }
